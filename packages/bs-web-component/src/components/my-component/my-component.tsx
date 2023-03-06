c4abde1c90d88dafc5192fbd98223eef4b35cd8d
Chen Yu <250187715@qq.com>
2023-03-06 18:15:29 +0800

    增加 echart  组件

--- packages/bs-web-component/src/components/my-component/my-component.tsx
+++ packages/bs-web-component/src/components/my-component/my-component.tsx
@@ -1,5 +1,6 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import {Component, Prop, h, Element} from '@stencil/core';
+import {format} from '../../utils/utils';
+import * as echarts from 'echarts';
 
 @Component({
   tag: 'my-component',
@@ -22,11 +23,40 @@ export class MyComponent {
    */
   @Prop() last: string;
 
+
+  @Element() el: HTMLElement;
+
   private getText(): string {
     return format(this.first, this.middle, this.last);
   }
 
+  componentDidLoad() {
+    //获取dom元素
+    const elementInShadowDom = this.el.shadowRoot.querySelector('#main');
+    //显示表格
+    let myChart = echarts.init(elementInShadowDom as HTMLElement);
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    });
+  }
+
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div id="main">Hello, World! I'm {this.getText()}</div>;
+    // return <div id="main"></div>;
   }
 }
