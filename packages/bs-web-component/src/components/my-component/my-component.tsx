c9950424b054061766f33c2aabea3e65dbc45dde
Chen Yu <250187715@qq.com>
2023-03-07 10:30:07 +0800

    调整一些模板

--- packages/bs-web-component/src/components/my-component/my-component.tsx
+++ packages/bs-web-component/src/components/my-component/my-component.tsx
@@ -30,33 +30,7 @@ export class MyComponent {
     return format(this.first, this.middle, this.last);
   }
 
-  componentDidLoad() {
-    //获取dom元素
-    // const elementInShadowDom = this.el.shadowRoot.querySelector('#main');
-    // //显示表格
-    // let myChart = echarts.init(elementInShadowDom as HTMLElement);
-    // // 绘制图表
-    // myChart.setOption({
-    //   title: {
-    //     text: 'ECharts 入门示例'
-    //   },
-    //   tooltip: {},
-    //   xAxis: {
-    //     data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-    //   },
-    //   yAxis: {},
-    //   series: [
-    //     {
-    //       name: '销量',
-    //       type: 'bar',
-    //       data: [5, 20, 36, 10, 10, 20]
-    //     }
-    //   ]
-    // });
-  }
-
   render() {
     return <div id="main">Hello, World! I'm {this.getText()}</div>;
-    // return <div id="main"></div>;
   }
 }
